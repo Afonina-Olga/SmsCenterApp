1df22e08784ccd38c3b0a9fa9e0b0434bf88a0b6
Afonina Olga <afonina@vcom.group>
2024-10-26 00:34:16 +0300

    DateTimeConverter

---
+++ SmsCenter.Api/Internal/Converters/DateTimeConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SmsCenter.Api.Internal.Converters;
+
+internal class DateTimeConverter : JsonConverter<DateTime>
+{
+    private DateTimeConverter() { }
+    public static readonly DateTimeConverter Instance = new();
+
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var str = reader.GetString() ?? throw new InvalidOperationException();
+        return DateTime.Parse(str);
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString("dd.MM.yyyy hh:mm:ss"));
+    }
+}
