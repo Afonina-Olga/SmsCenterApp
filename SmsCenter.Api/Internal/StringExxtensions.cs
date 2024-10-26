b5908779e1c8c17bd52319a28b69d5b32e790a1d
Afonina Olga <afonina@vcom.group>
2024-10-26 12:49:32 +0300

    String extensions

---
+++ SmsCenter.Api/Internal/StringExxtensions.cs
@@ -0,0 +1,19 @@
+﻿using Ardalis.GuardClauses;
+
+namespace SmsCenter.Api.Internal;
+
+internal static class StringExtensions
+{
+    private const char Separator = ',';
+
+    public static string ValidatePhone(this string phone) =>
+        Guard.Against.InvalidFormat(phone, nameof(phone), @"^\+7\d{10}$", "Phone format is not valid");
+
+    public static string ValidatePhonesAndJoin(this string[] phones) =>
+        string.Join(Separator, phones.Select(ValidatePhone));
+
+    public static string GetMessagesByPhoneList(this Dictionary<string, string> messagesByPhone) =>
+        string.Join(
+            Environment.NewLine,
+            messagesByPhone.Select(l => $"{l.Key}:{l.Value}"));
+}
