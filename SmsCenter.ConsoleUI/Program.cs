5592969e47e6807d647efc99162135363de7afbe
Afonina Olga <afonina@vcom.group>
2024-10-28 00:31:05 +0300

    Console UI

---
+++ SmsCenter.ConsoleUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using SmsCenter.Api;
+using SmsCenter.Api.Internal.Exceptions;
+using SmsCenter.Api.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.AddSmscService();
+
+
+var app = builder.Build();
+
+var service = app.Services.GetRequiredService<ISmsCenterService>();
+try
+{
+    var result = await service.SendSms("+79265718860", "Привет");
+    Console.WriteLine(result);
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.WriteLine("OK");
+}
+catch (SmsCenterException ex) {
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine(ex.Message);
+}
+catch (Exception ex) {
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine(ex.Message);
+}
