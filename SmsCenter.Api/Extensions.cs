741721e9c991cd5864efeff96cdb77bbbbdf682e
Afonina Olga <afonina@vcom.group>
2024-10-28 19:49:14 +0300

    Host builder extensions

---
+++ SmsCenter.Api/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using SmsCenter.Api.Options;
+using SmsCenter.Api.Providers;
+using SmsCenter.Api.Services;
+
+namespace SmsCenter.Api;
+
+public static class SmsCenterHostBuilderExtensions {
+    public static void AddSmscService(this WebApplicationBuilder builder) {
+        var services = builder.Services;
+        var configuration = builder.Configuration;
+        services.Configure<SmsCenterOptions>(configuration.GetSection("SmsCenter"),
+            options => options.BindNonPublicProperties = true);
+        services.AddSingleton<ISmsCenterProvider, SmsCenterProvider>();
+        services.AddSingleton<ISmsCenterService, SmsCenterService>();
+        services.AddHttpClient(SmsCenterProvider.HttpClientName,
+            config => { config.BaseAddress = new("https://smsc.ru"); });
+    }
+}
