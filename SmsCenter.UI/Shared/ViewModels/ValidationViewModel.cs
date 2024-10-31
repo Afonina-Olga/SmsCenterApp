13c8ba3623ed01cd41d0e0740c8d6332608ab447
Afonina Olga <afonina@vcom.group>
2024-10-31 21:14:02 +0300

    ValidationViewModel

---
+++ SmsCenter.UI/Shared/ViewModels/ValidationViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace SmsCenter.UI.Shared.ViewModels;
+
+public class ValidationViewModel : ViewModelBase, INotifyDataErrorInfo
+{
+    private readonly Dictionary<string, List<ValidationResult>> _errors = new();
+
+    public bool HasErrors => _errors.Any();
+
+    public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+    public IEnumerable GetErrors(string? propertyName)
+    {
+        return !string.IsNullOrEmpty(propertyName)
+            ? _errors.GetValueOrDefault(propertyName, null)
+            : new List<ValidationResult>();
+    }
+
+    public bool FindErrors(string propertyName) => _errors.ContainsKey(propertyName);
+
+    protected void ValidateProperty<T>(T value, [CallerMemberName] string propertyName = null)
+    {
+        if (string.IsNullOrEmpty(propertyName))
+            return;
+
+        var validationResults = new List<ValidationResult>();
+        var isValid = Validator.TryValidateProperty(
+            value,
+            new ValidationContext(this)
+            {
+                MemberName = propertyName
+            },
+            validationResults);
+
+        if (isValid)
+        {
+            _errors.Remove(propertyName);
+        }
+        else
+        {
+            _errors[propertyName] = validationResults;
+        }
+
+        ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+    }
+
+    protected void ClearErrors(string propertyName) => _errors.Remove(propertyName);
+
+    public void ClearErrors() => _errors.Clear();
+}
